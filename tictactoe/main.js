afdfaee653dcdbbbdf9f450a02c79dc58f1a74c3
Vijay Indalkar <vijayindalkar@gmail.com>
2021-09-16 13:58:36 +0530

    how it will work

--- tictactoe/main.js
+++ tictactoe/main.js
@@ -1,23 +1,56 @@
-function startGameSinglePlayer(){
-    var emptyboard = [
-        ["","",""],
-        ["","",""],
-        ["","", ""],
-        ["","",""],
-     ]; 
-     for(var i = 0; i < emptyboard.length; i++) {
-         document.write(emptyboard[i] + "<br>");
-     }    
+// First i will create an enpty array 
+// function to check weather is it empty or not 
+// function to assign value to position in array e.g [0][0] = 1, [0][1] = 2
+// ask users the position where to put  X and 0 
+// [(0,0)|(0,1)|(0,2)]       [1|2|3]
+// [(1,0)|(1,1)|(1,2)]       [4|5|6]  user input will be 1,2,3.....
+// [(2,0)|(2,1)|(2,2)]       [7|8|9]
+
+// function to check a set of pattern every time 
+// if the matches the following user Win   otherwise it will tie 
+
+var emptyboard = [
+    ["","",""],
+    ["","",""],
+    ["","",""],
+ ]; 
+
+function isEmpty(){
+    if (emptyboard == newBoard){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
+
+function startGameSinglePlayer(){
+    for(let posi = 0;posi <= 9;posi++){
+
+        winArrayX = window.prompt("X' turn | Enter Location : ");
+        
+        winArrayO = window.prompt("O' turn | Enter Location : ");
+    // for(let i = 0; i < emptyboard.length; i++) {
+         
+    //      document.write(emptyboard[i] + "<br>");
+    //  }
+}
+
+function toAssignInput(){
+// function to assign value to position in array e.g [0][0] = 1, [0][1] = 2
+
+}
+
+function tocheckPattern(){
+// function to check a set of pattern every time 
+}
+
+
+
 function startGameMultiplayer(){
-    var emptyboard = [
-        ["","",""],
-        ["","",""],
-        ["","",""],
-        ["","",""],
-     ]; 
-     for(var i = 0; i < emptyboard.length; i++) {
+    
+     for(let i = 0; i < emptyboard.length; i++) {
          document.write(emptyboard[i] + "<br>");
      }    
     
@@ -32,3 +65,4 @@ function nextStep(){
         startGameMultiplayer();
     }
 }
+
