79611233bc9d55068b73fc1b29acc724851b5bb3
Vijay Indalkar <vijayindalkar@gmail.com>
2021-09-16 12:19:49 +0530

    basic layout

--- tictactoe/main.js
+++ tictactoe/main.js
@@ -1,1 +1,34 @@
-s
+function startGameSinglePlayer(){
+    var emptyboard = [
+        ["","",""],
+        ["","",""],
+        ["","", ""],
+        ["","",""],
+     ]; 
+     for(var i = 0; i < emptyboard.length; i++) {
+         document.write(emptyboard[i] + "<br>");
+     }    
+}
+
+function startGameMultiplayer(){
+    var emptyboard = [
+        ["","",""],
+        ["","",""],
+        ["","",""],
+        ["","",""],
+     ]; 
+     for(var i = 0; i < emptyboard.length; i++) {
+         document.write(emptyboard[i] + "<br>");
+     }    
+    
+}
+
+function nextStep(){
+    let select = document.getElementById("numberOfPlayers");
+    if(select.options[select.selectedIndex].value == 1){
+        startGameSinglePlayer();
+    }
+    else{
+        startGameMultiplayer();
+    }
+}
